31a2ec3fc4df1eda2a19800cb479af8af199cee8
Guriya124 <guriyakumarisgvu@gmail.com>
2024-04-15 23:49:27 +0530

    Add Icons

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,9 @@
 import Dashboard from '../components/Dashboard'
 import DashBoardCard from '../components/DashBoardCard'
 import Icon from '../../public/image/invoice1.gif'
+import Icon1 from '../../public/image/cement1.gif'
+import Icon2 from '../../public/image/chem.gif'
+import Icon3 from '../../public/image/iron.gif'
 
 
 export default function Home() {
@@ -19,9 +22,9 @@ export default function Home() {
 
                     {/* second card */}
                     <DashBoardCard
-                        title="Total Invoice"
+                        title="Cement"
                         value="345"
-                        icon={Icon}
+                        icon={Icon1}
                         dynamicColor="card2"
                     // color="pinkToBlue"
 
@@ -29,17 +32,17 @@ export default function Home() {
                     {/* third card */}
 
                     <DashBoardCard
-                        title="Total Invoice"
+                        title="Chemical"
                         value="345"
-                        icon={Icon}
+                        icon={Icon2}
                         dynamicColor="card3"
                     // color="yellowToRed"
                     />
                     {/* Forth card */}
                     <DashBoardCard
-                        title="Total Invoice"
+                        title="Iron"
                         value="345"
-                        icon={Icon}
+                        icon={Icon3}
                         dynamicColor="card4"
                     // color="yellowToPink"
                     />
