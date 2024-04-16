6f63eae05dc5734f0773756f6561cae7fc93d13c
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-04-16 09:44:55 +0530

    Line Chart Component Created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,52 +1,56 @@
 
 import Dashboard from '../components/Dashboard'
 import DashBoardCard from '../components/DashBoardCard'
-import Icon from '../../public/image/invoice1.gif'
-import Icon1 from '../../public/image/cement1.gif'
-import Icon2 from '../../public/image/chem.gif'
-import Icon3 from '../../public/image/iron.gif'
-
+import Icon from '/image/invoice1.gif'
+import Icon1 from '/image/cement1.gif'
+import Icon2 from '/image/chem.gif'
+import Icon3 from '/image/iron.gif'
+import Paper from '@mui/material/Paper';
 
 export default function Home() {
     return (
         <div>
             <Dashboard>
-                <div className='flex flex-wrap gap-3'>
-                    <DashBoardCard
-                        title="Total Invoice"
-                        value="345"
-                        icon={Icon}
-                        // color="purpleToPink"
-                        dynamicColor="card1"
-                    />
+                <Paper elevation={3} className='p-10'>
+                    <div className='flex flex-wrap gap-3'>
+                        {/* second card */}
+                        <DashBoardCard
+                            title="Cement"
+                            value="345"
+                            icon={Icon1}
+                            dynamicColor="card2"
+                        />
+                        {/* third card */}
 
-                    {/* second card */}
-                    <DashBoardCard
-                        title="Cement"
-                        value="345"
-                        icon={Icon1}
-                        dynamicColor="card2"
-                    // color="pinkToBlue"
-
-                    />
-                    {/* third card */}
-
-                    <DashBoardCard
-                        title="Chemical"
-                        value="345"
-                        icon={Icon2}
-                        dynamicColor="card3"
-                    // color="yellowToRed"
-                    />
-                    {/* Forth card */}
-                    <DashBoardCard
-                        title="Iron"
-                        value="345"
-                        icon={Icon3}
-                        dynamicColor="card4"
-                    // color="yellowToPink"
-                    />
-
+                        <DashBoardCard
+                            title="Chemical"
+                            value="345"
+                            icon={Icon2}
+                            dynamicColor="card3"
+                        />
+                        {/* Forth card */}
+                        <DashBoardCard
+                            title="Iron"
+                            value="345"
+                            icon={Icon3}
+                            dynamicColor="card4"
+                        />
+                        {/* Change This for Stone  */}
+                        <DashBoardCard
+                            title="Total Invoice"
+                            value="345"
+                            icon={Icon}
+                            dynamicColor="card1"
+                        />
+                    </div>
+                </Paper>
+                <div className='flex flex-row gap-4'>
+                    <div>
+                        Use card here
+                    </div>
+                    <div>
+                        Use Line Chart Here
+                    </div>
                 </div>
             </Dashboard>
         </div>
