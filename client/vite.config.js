3d4e0f5eec9111981642c88db3e96ee7043d9568
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-04-22 01:10:52 +0530

    Update for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://bihari-traders-api.vercel.app/',
         secure: false,
       }
     }
