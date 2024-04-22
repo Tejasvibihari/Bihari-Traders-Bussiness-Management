63cf92d245e62067bb19019fd24249474cd68152
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-04-22 10:16:20 +0530

    Update

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://bihari-traders-api.vercel.app',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  plugins: [react()]
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://bihari-traders-api.vercel.app',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     }
+  //   }
+  // }
 });
