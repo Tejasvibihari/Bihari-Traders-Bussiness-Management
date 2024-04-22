55ef7e42a758d2ebf21e8edd93f1bb801b6649cb
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-04-22 23:56:24 +0530

    Update

--- client/src/service/axiosClient.js
+++ client/src/service/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-    baseURL: 'https://bihari-traders-api.vercel.app',
+    baseURL: 'https://bihari-traders-api.onrender.com',
     timeout: 10000
 });
 
