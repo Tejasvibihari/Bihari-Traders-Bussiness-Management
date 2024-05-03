cc85875f2b9a2519d7319f1fde9c454097c0a23c
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-05-03 21:38:44 +0530

    Update  Add Invoice Form State fro storing the date in dd-mm-yyyy formate

--- client/src/components/AddInvoiceForm.jsx
+++ client/src/components/AddInvoiceForm.jsx
@@ -45,7 +45,10 @@ export default function AddInvoiceForm() {
                 break;
             case 'date': {
                 const date = new Date(value);
-                const dateString = date.toISOString().slice(0, 10);
+                const day = String(date.getDate()).padStart(2, '0');
+                const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based in JavaScript
+                const year = date.getFullYear();
+                const dateString = `${day}-${month}-${year}`;
                 setDate(dateString);
                 break;
             }
